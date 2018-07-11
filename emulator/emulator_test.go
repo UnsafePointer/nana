82d9a2355b1a6e69e147f78a2c7ebec1fce8ce87
Renzo Crisostomo <renzo@crisostomo.me>
2018-07-11 17:34:09 +0200

    Add NewEmulator constructor function to Emulator
    * Used to initialize default values
    * Add: ProgramCounter, StackPointer and ROM map

---
+++ emulator/emulator_test.go
@@ -0,0 +1,35 @@
+package emulator_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	. "github.com/Ruenzuo/nana/emulator"
+)
+
+var _ = Describe("Emulator", func() {
+	var (
+		emulator Emulator
+	)
+
+	BeforeEach(func() {
+		emulator = *NewEmulator()
+	})
+
+	Describe("verifying the initial values", func() {
+		Context("when using new instance", func() {
+			It("should contain the right initial values", func() {
+				// Registers
+				Expect(emulator.AF.Value()).To(Equal(uint16(0x01B0)))
+				Expect(emulator.BC.Value()).To(Equal(uint16(0x0013)))
+				Expect(emulator.DE.Value()).To(Equal(uint16(0x00D8)))
+				Expect(emulator.HL.Value()).To(Equal(uint16(0x014D)))
+				// SP & PC
+				Expect(emulator.StackPointer.Value()).To(Equal(uint16(0xFFFE)))
+				Expect(emulator.ProgramCounter).To(Equal(uint16(0x100)))
+				// ROM (only one address)
+				Expect(emulator.ROM[0xFF19]).To(Equal(uint8(0xBF)))
+			})
+		})
+	})
+})
--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 
 func main() {
 	gameArg := os.Args[1]
-
-	e := emulator.Emulator{}
+	e := emulator.NewEmulator()
 	e.LoadCartridge(gameArg)
 }
