f3a45121ad92d76f99224aaa8f16670422469aca
Renzo Crisostomo <renzo@crisostomo.me>
2018-07-12 19:20:35 +0200

    Rename instruction set functions
    * CPU8BitLoad -> CPU8BitRegisterMemoryLoad
    * CPURegLoad -> CPU8BitRegisterLoad

--- emulator/instruction_set.go
+++ emulator/instruction_set.go
@@ -1,13 +1,13 @@
 package emulator
 
-func (e *Emulator) CPU8BitLoad(r *Register8Bit) int {
+func (e *Emulator) CPU8BitRegisterMemoryLoad(r *Register8Bit) int {
 	value := e.ReadMemory(e.ProgramCounter.Value())
 	r.SetValue(value)
 	e.ProgramCounter.Increment()
 	return 8
 }
 
-func (e *Emulator) CPURegLoad(r *Register8Bit, v Register8Bit) int {
+func (e Emulator) CPU8BitRegisterLoad(r *Register8Bit, v Register8Bit) int {
 	r.SetValue(v.Value())
 	return 4
 }
