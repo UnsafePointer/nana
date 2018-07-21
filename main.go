53e2a3e3c4a591f91bf1543f51faa79ec2232e45
Renzo Crisostomo <renzo@crisostomo.me>
2018-07-21 19:31:07 +0200

    Add graphics rendering

--- main.go
+++ main.go
@@ -1,23 +1,47 @@
 package main
 
 import (
+	"fmt"
 	"os"
-	"time"
 
 	"github.com/Ruenzuo/nana/emulator"
+	"github.com/hajimehoshi/ebiten"
+	"github.com/hajimehoshi/ebiten/ebitenutil"
 )
+
+const (
+	width  = 160
+	height = 144
+	scale  = 10
+)
+
+var (
+	e *emulator.Emulator
+)
+
+func update(screen *ebiten.Image) error {
+	e.EmulateFrame()
+	pixels := make([]byte, width*height*4)
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			position := y*width + x
+			pixels[(position*4 + 0)] = e.ScreenData[x][y][0]
+			pixels[(position*4 + 1)] = e.ScreenData[x][y][1]
+			pixels[(position*4 + 2)] = e.ScreenData[x][y][2]
+			pixels[(position*4 + 3)] = 255
+		}
+	}
+	screen.ReplacePixels(pixels)
+	ebitenutil.DebugPrint(screen, fmt.Sprintf("FPS: %f", ebiten.CurrentFPS()))
+	return nil
+}
 
 func main() {
 	gameArg := os.Args[1]
 	_, okDebug := os.LookupEnv("DEBUG")
-	e := emulator.NewEmulator(okDebug)
+	e = emulator.NewEmulator(okDebug)
 	e.LoadCartridge(gameArg)
-	ticker := time.NewTicker(time.Second / 60)
-	go func() {
-		for range ticker.C {
-			e.EmulateFrame()
-		}
-	}()
-	time.Sleep(time.Millisecond * 10000)
-	ticker.Stop()
+	if err := ebiten.Run(update, width, height, scale, "nana"); err != nil {
+		panic(err)
+	}
 }
