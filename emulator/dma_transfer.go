fc6fd1061fd67e6eb6c3c005d41960a014aacde8
Renzo Crisostomo <renzo@crisostomo.me>
2018-08-04 21:40:01 +0200

    Fix uint8 overflow

--- emulator/dma_transfer.go
+++ emulator/dma_transfer.go
@@ -1,7 +1,7 @@
 package emulator
 
 func (e *Emulator) DMATransfer(data uint8) {
-	address := uint16(data << 8)
+	address := uint16(data) << 8
 	for i := uint16(0); i < 0xA0; i++ {
 		data := e.ReadMemory8Bit(address + i)
 		e.WriteMemory(0xFE00+i, data)
