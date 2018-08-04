dfb56fabb544b925c0a6eabe3fc6bfc5968bf5d3
Renzo Crisostomo <renzo@crisostomo.me>
2018-08-04 21:55:45 +0200

    Add log message for DMA transfer

--- emulator/dma_transfer.go
+++ emulator/dma_transfer.go
@@ -1,6 +1,11 @@
 package emulator
 
+import (
+	"fmt"
+)
+
 func (e *Emulator) DMATransfer(data uint8) {
+	e.LogMessage(fmt.Sprintf("DMA Transfer: %#02x", data))
 	address := uint16(data) << 8
 	for i := uint16(0); i < 0xA0; i++ {
 		data := e.ReadMemory8Bit(address + i)
